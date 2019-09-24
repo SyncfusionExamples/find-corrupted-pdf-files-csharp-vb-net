07172b7a20cf26a15833665e929a6dd9876f79e8
Chinnu Muniyappan <chinnu.muniyappan@syncfusion.com>
2019-09-24 14:57:36 +0530

    alignment issue fix.

--- find-corrupted-pdf-file-demo/Program.cs
+++ find-corrupted-pdf-file-demo/Program.cs
@@ -11,31 +11,31 @@ namespace find_corrupted_pdf_file_demo
         static void Main(string[] args)
         {
             //Load the PDF file as stream
-using (FileStream pdfStream = new FileStream(@"..\..\..\PDF-Files\input-open-repair.pdf", FileMode.Open, FileAccess.Read))
-{
-    //Create a new instance of PDF document syntax analyzer.
-    PdfDocumentAnalyzer analyzer = new PdfDocumentAnalyzer(pdfStream);
-    //Analyze the syntax and return the results
-    SyntaxAnalyzerResult analyzerResult = analyzer.AnalyzeSyntax();
+            using (FileStream pdfStream = new FileStream(@"..\..\..\PDF-Files\input-open-repair.pdf", FileMode.Open, FileAccess.Read))
+            {
+                //Create a new instance of PDF document syntax analyzer.
+                PdfDocumentAnalyzer analyzer = new PdfDocumentAnalyzer(pdfStream);
+                //Analyze the syntax and return the results
+                SyntaxAnalyzerResult analyzerResult = analyzer.AnalyzeSyntax();
 
-    //Check whether the document is corrupted or not
-    if (analyzerResult.IsCorrupted)
-    {
-        StringBuilder strBuilder = new StringBuilder();
-        strBuilder.AppendLine("The PDF document is corrupted.");
-        int count = 1;
-        foreach (PdfException exception in analyzerResult.Errors)
-        {
-            strBuilder.AppendLine(count++.ToString() + ": " + exception.Message);
-        }
-        Console.WriteLine(strBuilder);
-    }
-    else
-    {
-        Console.WriteLine("No syntax error found in the provided PDF document");
-    }
-    analyzer.Close();
-}   
+                //Check whether the document is corrupted or not
+                if (analyzerResult.IsCorrupted)
+                {
+                    StringBuilder strBuilder = new StringBuilder();
+                    strBuilder.AppendLine("The PDF document is corrupted.");
+                    int count = 1;
+                    foreach (PdfException exception in analyzerResult.Errors)
+                    {
+                        strBuilder.AppendLine(count++.ToString() + ": " + exception.Message);
+                    }
+                    Console.WriteLine(strBuilder);
+                }
+                else
+                {
+                    Console.WriteLine("No syntax error found in the provided PDF document");
+                }
+                analyzer.Close();
+            }
         }
     }
 }
