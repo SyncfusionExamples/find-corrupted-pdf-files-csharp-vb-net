b837cbf108f4fa3bdea6987e5656474c51b0d5b6
Chinnu Muniyappan <chinnu.muniyappan@syncfusion.com>
2019-09-24 14:46:33 +0530

    Added sample for find a corrupted PDF file.

--- find-corrupted-pdf-file-demo/Program.cs
+++ find-corrupted-pdf-file-demo/Program.cs
@@ -1,23 +1,17 @@
-# How to find the corrupted PDF files in C# using Syncfusion PDF
-
-Syncfusion PDF Library provides support to find the existing PDF document corruptions and provides the corruption details
-
-## Finding the corrupted PDF document
-
-Install the [Syncfusion.Pdf.Net.Core](https://www.nuget.org/packages/Syncfusion.Pdf.Net.Core/) NuGet package as a reference to your .NET Core applications from [NuGet.org](https://www.nuget.org/).
-
-The following namespace should be included in the application:
-
-```C#
-using Syncfusion.Pdf;
+﻿using Syncfusion.Pdf;
 using Syncfusion.Pdf.Parsing;
-```
+using System;
+using System.IO;
+using System.Text;
 
-
-The following code snippet explains how to find the corrupted PDF document.
-
-```C#
-using (FileStream pdfStream = new FileStream(@"..\..\..\PDF-Files\barcode.pdf", FileMode.Open, FileAccess.Read))
+namespace find_corrupted_pdf_file_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load the PDF file as stream
+using (FileStream pdfStream = new FileStream(@"..\..\..\PDF-Files\input-open-repair.pdf", FileMode.Open, FileAccess.Read))
 {
     //Create a new instance of PDF document syntax analyzer.
     PdfDocumentAnalyzer analyzer = new PdfDocumentAnalyzer(pdfStream);
@@ -41,5 +35,7 @@ using (FileStream pdfStream = new FileStream(@"..\..\..\PDF-Files\barcode.pdf", 
         Console.WriteLine("No syntax error found in the provided PDF document");
     }
     analyzer.Close();
-} 
-```
+}   
+        }
+    }
+}
